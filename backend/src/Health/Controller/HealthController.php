539d2b692df76fd576904ebe721375f8cd263498
rene <rene.ruprecht.396@gmail.com>
2026-05-09 14:12:38 +0200

    feat(backend): add health endpoint with json response

---
+++ backend/src/Health/Controller/HealthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Health\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+
+  #[Route(path: '/health', name: 'health', methods: ['GET'])]
+  public function health(): JsonResponse
+  {
+    return new JsonResponse(
+      [
+        'status' => 'ok',
+        'service' => 'auth-service',
+        'timestamp' => time()
+
+      ],
+      Response::HTTP_OK
+    );
+  }
+}
